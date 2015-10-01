ba989bdf6094c3ff1a93527a33281f1c6117489f
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-01 15:37:20 +0200

    ansible

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd $HOME/
-git clone git://github.com/ansible/ansible.git --recursive
-cd ./ansible
+apt-get install git
+apt-get install python2.7
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible
 source ./hacking/env-setup
 echo "[ARTiMED_host_servers]" > $HOME/ansible/ansible_hosts
-echo "MACHINE WHERE GALAXY VM SHOULD BE INSTALLED" >> $HOME/ansible/ansible_hosts
+echo "127.0.0.1" >> $HOME/ansible/ansible_hosts
 export ANSIBLE_INVENTORY=$HOME/ansible/ansible_hosts
 ansible-playbook ARTiMED.yml --become-user=root
