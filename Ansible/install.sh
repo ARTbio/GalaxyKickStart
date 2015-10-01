73e7bb167671db1b6834b6cb7c4ba1926de6c6b2
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-01 17:10:24 +0200

    ansible

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-cd $HOME/
-apt-get install git
-apt-get install python2.7
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible
-source ./hacking/env-setup
-echo "[ARTiMED_host_servers]" > $HOME/ansible/ansible_hosts
-echo "127.0.0.1" >> $HOME/ansible/ansible_hosts
-export ANSIBLE_INVENTORY=$HOME/ansible/ansible_hosts
-ansible-playbook ARTiMED.yml --become-user=root
+vm_dir=galaxy_install
+mkdir $HOME/$vm_dir
+cd $HOME/$vm_dir
+
+sudo apt-get install git python2.7 software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+echo "[ARTiMED_host_servers]" > $HOME/$vm_dir/ansible_hosts
+echo "127.0.0.1" >> $HOME/$vm_dir/ansible_hosts
+export ANSIBLE_INVENTORY=$HOME/$vm_dir/ansible_hosts
+#git clone the other 3 files (ARTiMED.yml, galaxy.yml, Vagrantfile) 
+sed "s/???vm_dir???/$vm_dir/" ARTiMED.prototype.yml > ARTiMED.yml
+ansible-playbook $HOME/$vm_dir/ARTiMED.yml --become-user=root
