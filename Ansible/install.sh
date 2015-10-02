cf6360b6177ae7d136d0349fc37e2dd13f2db938
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-02 11:51:13 +0200

    ansible

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -11,6 +11,8 @@ sudo apt-get install ansible
 echo "[ARTiMED_host_servers]" > $HOME/$vm_dir/ansible_hosts
 echo "127.0.0.1" >> $HOME/$vm_dir/ansible_hosts
 export ANSIBLE_INVENTORY=$HOME/$vm_dir/ansible_hosts
-#git clone the other 3 files (ARTiMED.yml, galaxy.yml, Vagrantfile) 
-sed "s/???vm_dir???/$vm_dir/" ARTiMED.prototype.yml > ARTiMED.yml
-ansible-playbook $HOME/$vm_dir/ARTiMED.yml --become-user=root
+if git clone https://github.com/fabiorjvieira/general.git 
+then
+	sed "s/???vm_dir???/$vm_dir/" ARTiMED.prototype.yml > ARTiMED.yml
+	ansible-playbook $HOME/$vm_dir/ARTiMED.yml --become-user=root
+fi
