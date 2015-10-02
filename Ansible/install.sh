26d3ea2cfdde4581ad8ac54192ee33282d2789c2
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-02 17:44:11 +0200

    ok

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -18,7 +18,7 @@ echo "127.0.0.1" >> $vm_dir/ansible_hosts
 if git clone https://github.com/fabiorjvieira/general.git 
 then
 	mv -f $vm_dir/general/Ansible/* $vm_dir/; rm -rf $vm_dir/general/ #to be removed, however git must be corrected
-	sudo ansible-playbook -i $vm_dir/ansible_hosts "-e 'VM_DIR=$vm_dir'" $vm_dir/ARTiMED.yml
+	sudo ansible-playbook -i $vm_dir/ansible_hosts -e VM_DIR=$vm_dir $vm_dir/ARTiMED.yml
 fi
 
 
