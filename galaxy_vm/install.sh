935f7151f417484a02170932caec480da48769e4
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-08 12:27:51 +0200

    Fixing vagrant task.

--- galaxy_vm/install.sh
+++ galaxy_vm/install.sh
@@ -12,12 +12,10 @@ then
 	galaxy_relative_dir="galaxy_install"
 fi
 
-cd
 export vm_dir=$HOME/$galaxy_relative_dir
 mkdir -p $vm_dir
-cd $vm_dir
 sudo ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
-sudo cp -v /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+sudo cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 sudo apt-get install vim git python2.7 software-properties-common -y
 sudo apt-get install ansible -y
@@ -27,7 +25,6 @@ echo "[ARTiMED_host_servers]" >> $vm_dir/ansible_hosts
 echo "127.0.0.1" >> $vm_dir/ansible_hosts
 if git clone $git_repo 
 then
-	cd $galaxy_relative_dir
 	ssh-keygen -f "$HOME/.ssh/known_hosts" -R [localhost]:2222
-	sudo ansible-playbook -vvvv -i $vm_dir/ansible_hosts -e "VM_DIR=$vm_dir LOCAL_USER=$USER" $vm_dir/ARTiMED.yml
+	sudo ansible-playbook -vvvv -i $vm_dir/ansible_hosts -e "VM_DIR=$vm_dir LOCAL_USER=$USER" $vm_dir/ansible-artimed/ARTiMED.yml
 fi
