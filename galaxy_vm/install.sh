99771b7251de83e554e393e4253b92eb9594f146
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-21 12:29:23 +0200

    Galaxy installation works fine, means that galaxy.yml passed. However, if in a linux host if locales are not well configured postgresql will fail to create master database and the installation will also fail ([source test/locales.sh] and [pg_createcluster  master] solve it, but cannot be issue between roles). Maybe ansible handlers and [meta: flush_handlers] are the solution but I could not get it to work yet. Tools installation works fine too.

--- galaxy_vm/install.sh
+++ galaxy_vm/install.sh
@@ -2,28 +2,16 @@
 #Parsing parameters 
 if [ "$1" == "-h" ]
 then
-	echo "Just one optional parameter: the configuration file."
+	echo "Just one optional parameter: the git repository."
 	exit
 fi
 
-if [ "$1" != "" ]
-then
-	while read parameter
-	do
-		tag=´echo $parameter | cut -f 1 -d "=" ´
-		if [ "$tag" == "artimed_git_repo" ]; then artimed_git_repo=´echo $parameter | cut -f 2 -d "=" ´; 
-		elif [ "$tag" == "artimed_vm_relative_dir" ]; then artimed_vm_relative_dir=´echo $parameter | cut -f 2 -d "=" ´;
-		else echo "Invalid parameter in configuration file: $parameter"; fi
-	done < $1
-fi
+artimed_git_repo=$1
+artimed_vm_relative_dir="ansible-artimed/"
 
 if [ "$artimed_git_repo" == "" ]
 then
 	artimed_git_repo="https://github.com/fabiorjvieira/ansible-artimed.git"
-fi
-if [ "$artimed_vm_relative_dir" == "" ]
-then
-	artimed_vm_relative_dir="ansible-artimed/"
 fi
 
 #Dependencies
