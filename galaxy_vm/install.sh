0de83007bbd3f6204faf104dcd65d60c95420d9e
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-10-30 17:00:14 +0100

    Unifing the installation for VM/Cloud node/Real Machine

--- galaxy_vm/install.sh
+++ galaxy_vm/install.sh
@@ -13,31 +13,19 @@ then
 	artimed_git_repo="https://github.com/fabiorjvieira/ansible-artimed.git"
 fi
 
-#Test Dependencies
+#install Dependencies
 echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git."
-
-gitVersion=`git --version | grep -i "^git"`
-if [ "$gitVersion" == "" ]
-then
-	echo "git not present, please install it."
-	exit 1
-fi
-
-ansibleVersion=`ansible --version | grep -i "^ansible" | sed "s/ansible //"`
-ansibleVersion=`printf "$ansibleVersion\n1.8.0\n" | sort -n | head -n1`
-if [ "$ansibleVersion" != "1.8.0" ]
-then
-	echo "ansible (>= 1.8) not present, please install it."
-	exit 1
-fi
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install software-properties-common git openssh-client ansible	
 
 #Installation
 if git clone --recursive $artimed_git_repo
 then
 	cd ansible-artimed/galaxy_vm/
-	sudo INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml
+	sudo INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml
 	echo "Wait until galaxy provide the web service on http://localhost:8080"
 	echo -n "Press any key to install Galaxy tools..."
 	read
-	sudo INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml
+	sudo INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml
 fi
