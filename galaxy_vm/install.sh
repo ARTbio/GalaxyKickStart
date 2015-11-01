86b3f80ba4c39549150f8b2a779842695035b3b0
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-11-01 13:35:48 +0100

    Tested on amazon cloud by running bash install.sh

--- galaxy_vm/install.sh
+++ galaxy_vm/install.sh
@@ -15,17 +15,29 @@ fi
 
 #install Dependencies
 echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git."
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install software-properties-common git openssh-client ansible	
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt-get install software-properties-common git openssh-client ansible -y	
+
+#To be sure that the current user can ssh to the localhost without password 
+if cat /dev/zero | ssh-keygen -q -N ""
+then
+	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+else
+	k=`grep -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys | wc -l`
+	if [ "$k" == "0" ]
+	then
+		cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+	fi
+fi
 
 #Installation
 if git clone --recursive $artimed_git_repo
 then
 	cd ansible-artimed/galaxy_vm/
-	sudo INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml
+	INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml -vvvv
 	echo "Wait until galaxy provide the web service on http://localhost:8080"
 	echo -n "Press any key to install Galaxy tools..."
 	read
-	sudo INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml
+	INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml -vvvv
 fi
