2138678b7226a6ab37634be174d0edbb63242dc0
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-11-04 13:48:19 +0100

    Going back to supervisor

--- galaxy_vm/install_vm.sh
+++ galaxy_vm/install_vm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Test Dependencies
 echo "Minimum requirements: OpenSSH client, Ansible >=1.8, Vagrant >=1.7.4, Virtual Box (compatible with vagrant - see vagrant site) and git."
-#INSTALL_USER=vagrant PLAYBOOK="galaxy.yml" VAGRANT_LOG=info vagrant up
-#INSTALL_USER=vagrant PLAYBOOK="tools.yml" VAGRANT_LOG=info vagrant provision
-
 vagrant up
 vagrant ssh
+#INSTALL_USER=vagrant PLAYBOOK="../galaxy/galaxy.yml" VAGRANT_LOG=info vagrant provision
+#INSTALL_USER=vagrant PLAYBOOK="../galaxy/tools.yml" VAGRANT_LOG=info vagrant provision
