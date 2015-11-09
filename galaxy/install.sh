4dbc6e4661d4f81b683720883d45a391c13773fa
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-11-09 17:43:19 +0100

    Galaxy service fixed, but it do not start automatically because postgresql and proftpd.

--- galaxy/install.sh
+++ galaxy/install.sh
@@ -42,24 +42,42 @@ else
 fi
 
 #install Dependencies
-echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git."
+echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git (this script will try to install these)."
 if [ "$IM" == "apt" ]
 then
+	sudo apt-get install software-properties-common -y
 	sudo apt-add-repository ppa:ansible/ansible -y
 	sudo apt-get update -y
-	sudo apt-get install software-properties-common git openssh-client ansible -y
+	sudo apt-get install git openssh-client ansible -y
 fi
 
-#To be sure that the current user can ssh to the localhost without password 
+echo "To be sure that the current user can ssh to the localhost without password" 
 if cat /dev/zero | ssh-keygen -q -N ""
 then
+	echo "new key created"
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 else
-	k=`grep -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys | wc -l`
+	echo "Old key exists"
+        if [ -r ~/.ssh/authorized_keys ]
+        then
+           echo "Authorized key exists"
+	   k=`grep -c -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys`
+        else
+           k=0
+        fi
 	if [ "$k" == "0" ]
 	then
+		echo "Old key added"
 		cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	fi
+fi
+
+if ssh localhost "echo ''"; 
+then 
+	echo "This user has permission to ssh directly to the localhost without any user input."; 
+else 
+	echo "This user does not have permission to ssh directly to the localhost without any user input, so this script cannot continue.";
+	exit 1
 fi
 
 #Installation
