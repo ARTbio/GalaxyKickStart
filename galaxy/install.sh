ef24de5009990d39fe7529de80ef6ca2c53d4d3c
Fabio Rocha Jimenez Vieira <fjimenez@cos.ufrj.br>
2015-11-10 09:57:32 +0100

    Update install.sh

--- galaxy/install.sh
+++ galaxy/install.sh
@@ -2,7 +2,7 @@
 #Parsing parameters 
 if [ "$1" == "-h" ]
 then
-	echo "Just one optional parameter: the git repository."
+	echo "Just 4 optional parameters: the git repository, galaxy user, galaxy port and ftp port."
 	exit 0
 fi
 
@@ -10,6 +10,24 @@ artimed_git_repo=$1
 if [ "$artimed_git_repo" == "" ]
 then
 	artimed_git_repo="https://github.com/artbio/ansible-artimed.git"
+fi
+
+galaxy_user=$2
+if [ "$galaxy_user" == "" ]
+then
+	galaxy_user="galaxy"
+fi
+
+galaxy_port=$2
+if [ "$galaxy_port" == "" ]
+then
+	galaxy_port="8080"
+fi
+
+ftp_port=$2
+if [ "$ftp_port" == "" ]
+then
+	ftp_port="2121"
 fi
 
 OS=`head -n1 /etc/issue | cut -d " " -f 1`
@@ -24,33 +42,51 @@ else
 fi
 
 #install Dependencies
-echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git."
+echo "Minimum requirements: OpenSSH client, Ansible >=1.8 and git (this script will try to install these)."
 if [ "$IM" == "apt" ]
 then
+	sudo apt-get install software-properties-common -y
 	sudo apt-add-repository ppa:ansible/ansible -y
 	sudo apt-get update -y
-	sudo apt-get install software-properties-common git openssh-client ansible -y
+	sudo apt-get install git openssh-client ansible -y
 fi
 
-#To be sure that the current user can ssh to the localhost without password 
+echo "To be sure that the current user can ssh to the localhost without password" 
 if cat /dev/zero | ssh-keygen -q -N ""
 then
+	echo "new key created"
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 else
-	k=`grep -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys | wc -l`
+	echo "Old key exists"
+        if [ -r ~/.ssh/authorized_keys ]
+        then
+           echo "Authorized key exists"
+	   k=`grep -c -f ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys`
+        else
+           k=0
+        fi
 	if [ "$k" == "0" ]
 	then
+		echo "Old key added"
 		cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	fi
+fi
+
+if ssh localhost "echo ''"; 
+then 
+	echo "This user has permission to ssh directly to the localhost without any user input."; 
+else 
+	echo "This user does not have permission to ssh directly to the localhost without any user input, so this script cannot continue.";
+	exit 1
 fi
 
 #Installation
 if git clone --recursive $artimed_git_repo
 then
 	cd ansible-artimed/galaxy/
-	INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml -vvvv
+	FTP_PORT=$ftp_port GALAXY_USER=$galaxy_user GALAXY_PORT=$galaxy_port INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml -vvvv
 	echo "Wait until galaxy provide the web service on http://localhost"
 	echo -n "Press control+c to stop here or enter key to install Galaxy tools..."
 	read
-	INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml -vvvv
+	GALAXY_USER=$galaxy_user GALAXY_PORT=$galaxy_port INSTALL_USER=$USER ansible-playbook -i "localhost," tools.yml -vvvv
 fi
