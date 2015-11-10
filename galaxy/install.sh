41fba2a4c7ca22a3fa8da80ad0d2ddc2dc79662b
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-11-10 12:18:15 +0100

    Tring to use supervisor, slurm and uwsgi through galaxy role.

--- galaxy/install.sh
+++ galaxy/install.sh
@@ -84,7 +84,8 @@ fi
 if git clone --recursive $artimed_git_repo
 then
 	cd ansible-artimed/galaxy/
-	FTP_PORT=$ftp_port GALAXY_USER=$galaxy_user GALAXY_PORT=$galaxy_port INSTALL_HOSTNAME=$HOSTNAME INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml -vvvv
+	hostIP=`hostname -I | cut -d " " -f 1`
+	FTP_PORT=$ftp_port GALAXY_USER=$galaxy_user GALAXY_PORT=$galaxy_port INSTALL_HOSTNAME=$hostIP INSTALL_USER=$USER ansible-playbook -i "localhost," galaxy.yml -vvvv
 	echo "Wait until galaxy provide the web service on http://localhost"
 	echo -n "Press control+c to stop here or enter key to install Galaxy tools..."
 	read
