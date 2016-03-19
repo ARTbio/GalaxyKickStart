247a2b864c8613e461860a99b2d5027da88bc10e
Marius van den Beek <m.vandenbeek@gmail.com>
2016-03-19 12:35:02 +0100

    Also tag image as latest

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ docker tag galaxy_kickstart artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
 docker tag galaxy_kickstart artbio/galaxy-kickstart-base:latest
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
+docker push artbio/galaxy-kickstart-base:latest
