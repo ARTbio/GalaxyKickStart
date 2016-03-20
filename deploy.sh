ffe3971f6843e044ffbf06fc9e8fc465cb7ad704
Marius van den Beek <m.vandenbeek@gmail.com>
2016-03-20 11:05:07 +0100

    Try all dockerhub related commands twice.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 docker tag galaxy_kickstart artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
 docker tag galaxy_kickstart artbio/galaxy-kickstart-base:latest
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
-docker push artbio/galaxy-kickstart-base:latest
+LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
+$LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
+docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
+docker push artbio/galaxy-kickstart-base:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
