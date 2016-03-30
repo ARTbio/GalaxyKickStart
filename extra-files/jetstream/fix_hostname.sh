43aa9c4ba9b28d4804f44813090363e860270d40
Enis Afgan <afgane@gmail.com>
2016-03-30 13:30:01 -0400

    Fix fix_hostname script

--- extra-files/jetstream/fix_hostname.sh
+++ extra-files/jetstream/fix_hostname.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-service restart hostname
-HOSTNAME=`hostname`
+HN=$(cat /etc/hostname)
 
 cat<<EOF > /etc/hosts
 # This file was automatically created by fix_hostname.sh script
-127.0.0.1 localhost $HOSTNAME
+127.0.0.1 localhost $HN
 
 # The following lines are desirable for IPv6 capable hosts
 ::1 ip6-localhost ip6-loopback
