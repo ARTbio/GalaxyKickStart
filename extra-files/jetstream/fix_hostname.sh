26e4f359f349e41c8726a01cb0f961988044e23b
Enis Afgan <afgane@gmail.com>
2016-08-12 16:06:14 -0400

    Use Galaxy project's CVMFS file system for reference data. Also, fix hostname setting on Jetstream so Slurm starts properly.

--- extra-files/jetstream/fix_hostname.sh
+++ extra-files/jetstream/fix_hostname.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-service hostname restart
 HN=$(cat /etc/hostname)
 
 cat<<EOF > /etc/hosts
@@ -14,3 +13,5 @@ ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 ff02::3 ip6-allhosts
 EOF
+
+service hostname restart
