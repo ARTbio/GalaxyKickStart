ef260fe0cae8017cefc9aa1be9929b90e978ce5b
drosofff <drosofff@gmail.com>
2017-05-01 16:34:45 +0200

    improve generate_tool_list_from_ga_workflow_files.py with install_resolver_dependencies: True

--- scripts/generate_tool_list_from_ga_workflow_files.py
+++ scripts/generate_tool_list_from_ga_workflow_files.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import yaml
 import json
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+import yaml
 
 
 def _parse_cli_options():
@@ -11,10 +11,11 @@ def _parse_cli_options():
     """
     parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter,
                             usage="python %(prog)s <options>",
-                            epilog="example:\n"
+                            epilog="Workflow files must have been exported from Galaxy release 16.04 or newer.\n\n"
+                                   "example:\n"
                                    "python %(prog)s -w workflow1 workflow2 -o mytool_list.yml -l my_panel_label\n"
                                    "Christophe Antoniewski <drosofff@gmail.com>\n"
-                                   "https://github.com/ARTbio/GalaxyKickStart/tree/master/scritps/generate_tool_list_from_ga_workflow_files.py")
+                                   "https://github.com/ARTbio/ansible-artimed/tree/master/extra-files/generate_tool_list_from_ga_workflow_files.py")
     parser.add_argument('-w', '--workflow',
                         dest="workflow_files",
                         required=True,
@@ -47,25 +48,46 @@ def translate_workflow_dictionary_to_tool_list(tool_dictionary, panel_label):
     tool_list = []
     for tool in starting_tool_list:
         sub_dic = {'name': tool['name'], 'owner': tool['owner'], 'revision': tool['changeset_revision'],
-                      'tool_panel_section_label': panel_label, 'tool_shed_url': 'https://'+tool['tool_shed']}
+                  'tool_panel_section_label': panel_label, 'tool_shed_url': 'https://'+tool['tool_shed'],
+                  'zinstall_resolver_dependencies': 'True'}
         tool_list.append(sub_dic)
     return tool_list
+
 
 def print_yaml_tool_list(tool_dictionary, output_file):
     with open(output_file, 'w') as F:
         F.write(yaml.safe_dump(tool_dictionary, default_flow_style=False))
+    lines = []
+    with open(output_file, 'r') as F:
+        for line in F:
+            line = line.replace("zinstall", "install")
+            line = line.replace("-", "\n-")
+            lines.append(line)
+    with open(output_file, 'w') as F:
+        for line in lines:
+            F.write(line)
     return
 
+
 def generate_tool_list_from_workflow(workflow_files, panel_label, output_file):
+    """
+
+    :rtype: object
+    """
     intermediate_tool_list = []
     for workflow in workflow_files:
-        workflow_dictionary = get_workflow_dictionary (workflow)
-        intermediate_tool_list += translate_workflow_dictionary_to_tool_list (workflow_dictionary, panel_label)
+        workflow_dictionary = get_workflow_dictionary(workflow)
+        intermediate_tool_list += translate_workflow_dictionary_to_tool_list(workflow_dictionary, panel_label)
     reduced_tool_list = list({v['revision']: v for v in intermediate_tool_list}.values())
     convert_dic = {}
     convert_dic['tools'] = reduced_tool_list
     print_yaml_tool_list(convert_dic, output_file)
 
-if __name__ == "__main__":
+
+def main():
     options = _parse_cli_options()
     generate_tool_list_from_workflow(options.workflow_files, options.panel_label, options.output_file)
+
+
+if __name__ == "__main__":
+    main()
