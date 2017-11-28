5d9f93ef1fea616529fcc85ba351f3383ff09a18
Christophe Antoniewski <drosofff@gmail.com>
2017-11-28 18:40:53 +0100

    suppress bioblend test for test

--- travis_scripts/script_ansible.sh
+++ travis_scripts/script_ansible.sh
@@ -4,7 +4,7 @@ ansible-playbook -i inventory_files/metavisitor galaxy.yml --connection=local --
 sleep 60s
 curl http://localhost:80/api/version| grep version_major
 date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
-sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
-cd $GALAXY_HOME &&
-bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
+#sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
+#cd $GALAXY_HOME &&
+#bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
 curl --fail $BIOBLEND_GALAXY_URL/api/version
