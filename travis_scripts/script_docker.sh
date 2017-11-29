49af9910353dda64568aaeab6f35d49238b2a09c
drosofff <drosofff@gmail.com>
2017-11-29 15:00:35 +0100

    remove debugging code

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -3,9 +3,6 @@ set -e
 sleep 60s
 docker logs $CID2
 curl http://localhost:8181/subdir/api/version| grep version_major
-echo $GALAXY_TRAVIS_USER
-echo $GALAXY_HOME
-echo $PATH
 sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
   cd $GALAXY_HOME &&
   bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
