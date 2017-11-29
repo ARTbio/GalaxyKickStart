c6a7441a4bae63a8496a07198bdfb6bbbd961212
drosofff <drosofff@gmail.com>
2017-11-29 09:53:45 +0100

    manually pin six to =1.10.0 for galaxy_travis_user

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -8,7 +8,7 @@ echo $GALAXY_HOME
 echo $PATH
 echo "export PATH=$GALAXY_HOME/.local/bin/:$PATH && cd $GALAXY_HOME && bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
 sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
-  cd $GALAXY_HOME &&
+  cd $GALAXY_HOME && pip install six===1.10.0 &&
   bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
 curl --fail $BIOBLEND_GALAXY_URL/api/version
 date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
