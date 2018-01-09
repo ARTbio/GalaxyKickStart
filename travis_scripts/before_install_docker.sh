c51b8abd3b1ea4a5f75c35f046da7a4c84890ba8
drosofff <drosofff@gmail.com>
2018-01-10 00:14:29 +0100

    more tweak on travis_tests to manage restart handler in various environments

--- travis_scripts/before_install_docker.sh
+++ travis_scripts/before_install_docker.sh
@@ -6,6 +6,7 @@ pip --version
 pip install ansible
 ansible-galaxy install -r requirements_roles.yml -p roles
 mv extra-files/metavisitor/metavisitor_tool_list.yml.fortestonly extra-files/metavisitor/metavisitor_tool_list.yml
+printf "galaxy_runs_in_docker: yes\ngalaxy_docker_container_id: standard\n" >> group_vars/all
 sudo groupadd -r $GALAXY_TRAVIS_USER -g $GALAXY_GID
 sudo useradd -u $GALAXY_UID -r -g $GALAXY_TRAVIS_USER -d $GALAXY_HOME -p travis_testing\
   -c "Galaxy user" $GALAXY_TRAVIS_USER
@@ -15,6 +16,7 @@ docker build -t metavisitor .
 sudo mkdir /export && sudo chown $GALAXY_UID:$GALAXY_GID /export
 sudo mkdir /export2 && sudo chown $GALAXY_UID:$GALAXY_GID /export2
 export STANDARD=`docker run -d \
+  --name standard
   -p 80:80 -p 8021:21 -p 8800:8800 --tmpfs /var/run/ \
   --privileged=true \
   -e GALAXY_CONFIG_ALLOW_USER_DATASET_PURGE=True \
