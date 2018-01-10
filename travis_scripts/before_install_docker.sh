c946f7387121fd6bc0be6da82586c6c9261b74ad
drosofff <drosofff@gmail.com>
2018-01-10 01:27:57 +0100

    fucked up the docker run statement. fixed with \

--- travis_scripts/before_install_docker.sh
+++ travis_scripts/before_install_docker.sh
@@ -16,7 +16,7 @@ docker build -t metavisitor .
 sudo mkdir /export && sudo chown $GALAXY_UID:$GALAXY_GID /export
 sudo mkdir /export2 && sudo chown $GALAXY_UID:$GALAXY_GID /export2
 export STANDARD=`docker run -d \
-  --name standard
+  --name standard \
   -p 80:80 -p 8021:21 -p 8800:8800 --tmpfs /var/run/ \
   --privileged=true \
   -e GALAXY_CONFIG_ALLOW_USER_DATASET_PURGE=True \
