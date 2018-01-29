ca59e064233c9898c9774e6967d2b2cebc9c6456
drosofff <drosofff@gmail.com>
2018-01-29 22:50:32 +0100

    revert Bioblend_url env to 8080. put /subdir prefix for  and test as is

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -7,7 +7,7 @@ curl http://localhost:8181/subdir/api/version| grep version_major
 sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
   cd $GALAXY_HOME &&
   bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
-curl --fail $BIOBLEND_GALAXY_URL/api/version
+curl --fail $BIOBLEND_GALAXY_URL/subdir/api/version
 date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 curl --fail ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 docker exec -it $CID1 supervisorctl status | grep proftpd | grep RUNNING
