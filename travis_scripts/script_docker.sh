08e2f9e1b894db21e0becee69ab6f7d6349a14de
drosofff <drosofff@gmail.com>
2018-01-29 23:17:20 +0100

    put Galaxy_travis_bioblend to localhost:8080/subdir

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -3,11 +3,12 @@ set -e
 echo -e "sleeping 120s, zzzzzz"
 sleep 120s
 # docker logs $CID2
+export $BIOBLEND_GALAXY_URL="http://localhost:8080/subdir"
 curl http://localhost:8181/subdir/api/version| grep version_major
 sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
   cd $GALAXY_HOME &&
   bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
-curl --fail $BIOBLEND_GALAXY_URL/subdir/api/version
+curl --fail $BIOBLEND_GALAXY_URL/api/version
 date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 curl --fail ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 docker exec -it $CID1 supervisorctl status | grep proftpd | grep RUNNING
