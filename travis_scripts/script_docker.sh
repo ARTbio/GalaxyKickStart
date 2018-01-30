438e3ce3eb1771d449bc3c7e179f2d616951404f
Christophe Antoniewski <drosofff@gmail.com>
2018-01-30 17:51:16 +0100

    revert to master testing to better understand bioblend-galaxy-tests

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -5,8 +5,8 @@ sleep 120s
 # docker logs $CID2
 #export $BIOBLEND_GALAXY_URL="http://localhost:8080/subdir"
 curl http://localhost:8181/subdir/api/version| grep version_major
+ls -la $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py
 sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
-  export BIOBLEND_GALAXY_URL='http://localhost:8080/subdir' &&
   cd $GALAXY_HOME &&
   bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
 curl --fail $BIOBLEND_GALAXY_URL/api/version
