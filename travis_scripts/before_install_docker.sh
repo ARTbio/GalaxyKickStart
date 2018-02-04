7fd48c54e6e9e1b2a5f0bd548c50fdc568926d9d
Christophe Antoniewski <drosofff@gmail.com>
2018-02-04 19:48:01 +0000

    switch docker tests to a Dockerfile.noinstalltool docker file

--- travis_scripts/before_install_docker.sh
+++ travis_scripts/before_install_docker.sh
@@ -3,13 +3,13 @@ set -e
 docker --version
 docker info
 pip install ansible==2.2.0.0
-ansible-galaxy install -r requirements_roles.yml -p roles
+ansible-galaxy install -r requirements_roles.yml -p roles -f
 sudo groupadd -r $GALAXY_TRAVIS_USER -g $GALAXY_GID
 sudo useradd -u $GALAXY_UID -r -g $GALAXY_TRAVIS_USER -d $GALAXY_HOME -p travis_testing\
   -c "Galaxy user" $GALAXY_TRAVIS_USER
 sudo mkdir $GALAXY_HOME
 sudo chown -R $GALAXY_TRAVIS_USER:$GALAXY_TRAVIS_USER $GALAXY_HOME
-docker build -t galaxy_kickstart .
+docker build -t galaxy_kickstart -f Dockerfile.noinstalltool .
 sudo mkdir /export && sudo chown $GALAXY_UID:$GALAXY_GID /export
 sudo mkdir /export2 && sudo chown $GALAXY_UID:$GALAXY_GID /export2
 export CID1=`docker run -d --privileged=true -p 80:80 -p 21:21\
