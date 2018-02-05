a62cf0822534e5d0b4e1dcc4b2690f68678d8bd0
Christophe Antoniewski <drosofff@gmail.com>
2018-02-05 12:19:56 +0100

    add a travis_wait statement for the docker metavisitor buit

--- travis_scripts/before_install_docker.sh
+++ travis_scripts/before_install_docker.sh
@@ -9,7 +9,7 @@ sudo useradd -u $GALAXY_UID -r -g $GALAXY_TRAVIS_USER -d $GALAXY_HOME -p travis_
   -c "Galaxy user" $GALAXY_TRAVIS_USER
 sudo mkdir $GALAXY_HOME
 sudo chown -R $GALAXY_TRAVIS_USER:$GALAXY_TRAVIS_USER $GALAXY_HOME
-docker build -t metavisitor -f Dockerfile.test .
+travis_wait docker build -t metavisitor -f Dockerfile.test .
 sudo mkdir /export && sudo chown $GALAXY_UID:$GALAXY_GID /export
 sudo mkdir /export2 && sudo chown $GALAXY_UID:$GALAXY_GID /export2
 export CID1=`docker run -d --privileged=true -p 80:80 -p 21:21\
