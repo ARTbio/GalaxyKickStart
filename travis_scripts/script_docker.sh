7062f9ad45245bf58d7cea0c094213ccbd4d8830
drosofff <drosofff@gmail.com>
2018-02-08 00:43:15 +0100

    last, adapt $CID3 to metavisitor docker image

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -17,7 +17,7 @@ date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:8021 --us
 curl --fail ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 docker exec -it $CID1 supervisorctl status | grep proftpd | grep RUNNING
 docker stop $CID1 $CID2 && docker rm $CID1 $CID2
-CID3=`docker run -d --privileged=true -p 8181:80 -e NAT_MASQUERADE=true -v /export2:/export galaxy_kickstart` && sleep 120s
+CID3=`docker run -d --privileged=true -p 8181:80 -e NAT_MASQUERADE=true -v /export2:/export metavisitor` && sleep 90s
 docker logs $CID3
 curl http://localhost:8181/api/version| grep version_major
 cd $TRAVIS_BUILD_DIR
