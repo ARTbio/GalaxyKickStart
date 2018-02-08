b97f7cde9cf9096db11ac48b8c0753eb34945050
drosofff <drosofff@gmail.com>
2018-02-08 01:01:37 +0100

    increasing sleep time

--- travis_scripts/script_docker.sh
+++ travis_scripts/script_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo -e "sleeping 90s, moving data to /export directory zzzzzz"
-sleep 90s
+sleep 120s
 docker logs $CID1
 echo -e "Testing CID1 $CID1"
 curl http://localhost:80/subdir/api/version| grep version_major
@@ -17,7 +17,7 @@ date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:8021 --us
 curl --fail ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 docker exec -it $CID1 supervisorctl status | grep proftpd | grep RUNNING
 docker stop $CID1 $CID2 && docker rm $CID1 $CID2
-CID3=`docker run -d --privileged=true -p 8181:80 -e NAT_MASQUERADE=true -v /export2:/export metavisitor` && sleep 90s
+CID3=`docker run -d --privileged=true -p 8181:80 -e NAT_MASQUERADE=true -v /export2:/export metavisitor` && sleep 120s
 docker logs $CID3
 curl http://localhost:8181/api/version| grep version_major
 cd $TRAVIS_BUILD_DIR
