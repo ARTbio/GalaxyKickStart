88c7d0daa2ec4267dbca18e4bce56dd877aa77e4
drosofff <drosofff@gmail.com>
2018-02-22 23:25:54 +0100

    reactivate deploy.sh script - documentation was not built anymore

--- travis_scripts/deploy.sh
+++ travis_scripts/deploy.sh
@@ -12,11 +12,3 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
 cd $SCRIPT_PATH
-
-# Next we upload to docker
-docker tag galaxy_kickstart artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
-docker tag galaxy_kickstart artbio/galaxy-kickstart-base:latest
-LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
-$LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
-docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
-docker push artbio/galaxy-kickstart-base:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
