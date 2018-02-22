813133e4a7f3f37a9fdd1a1b8792c452249721be
drosofff <drosofff@gmail.com>
2018-02-23 00:13:58 +0100

    put conditons in deploy.sh

--- travis_scripts/deploy.sh
+++ travis_scripts/deploy.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
-sudo pip install mkdocs
-mkdir mkdocs_build
-cd mkdocs_build
-# Initialize gh-pages checkout
-DATE=`date`
-git clone https://github.com/ARTbio/GalaxyKickStart.git
-cd GalaxyKickStart
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
-cd $SCRIPT_PATH
+if [ "$TRAVIS_JOB" = "ansible-test" ] && [ "$TRAVIS_EVENT_TYPE" = "pull_request"]; then
+    SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+    sudo pip install mkdocs
+    mkdir mkdocs_build
+    cd mkdocs_build
+    # Initialize gh-pages checkout
+    DATE=`date`
+    git clone https://github.com/ARTbio/GalaxyKickStart.git
+    cd GalaxyKickStart
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+    mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
+    cd $SCRIPT_PATH
+fi
