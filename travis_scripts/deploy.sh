987093fd4c00ec70a61f0803cd40a85812eff59d
drosofff <drosofff@gmail.com>
2018-02-23 11:52:21 +0100

    lint deploy.sh

--- travis_scripts/deploy.sh
+++ travis_scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-if [ "$TRAVIS_JOB" = "ansible-test" ] && [ "$TRAVIS_EVENT_TYPE" = "pull_request"]; then
+if [ $TRAVIS_JOB = "ansible-test" ] && [ $TRAVIS_EVENT_TYPE = "pull_request"]; then
     echo "Deploy mkdocs"
     SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
     sudo pip install mkdocs
