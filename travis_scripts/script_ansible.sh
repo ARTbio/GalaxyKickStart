78e3b5657dc283ff71885969ebc3d6805d7030fc
drosofff <drosofff@gmail.com>
2020-10-27 19:47:20 +0100

    the correct command for wait is... sleep

--- travis_scripts/script_ansible.sh
+++ travis_scripts/script_ansible.sh
@@ -3,7 +3,7 @@ set -e
 ansible-galaxy install -r requirements_roles.yml -p roles
 # sed -i '34d' roles/galaxyprojectdotorg.galaxy/tasks/database.yml  # remove temporary a line to unlock galaxy db build
 ansible-playbook -i inventory_files/galaxy-kickstart --skip-tags install_tools galaxy.yml
-time 60
+sleep 60
 ansible-playbook -i inventory_files/galaxy-kickstart --tasks install_tools galaxy.yml
 
 curl --fail $BIOBLEND_GALAXY_URL/api/version
