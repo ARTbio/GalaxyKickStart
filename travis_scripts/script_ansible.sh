a812cc12e5871553d72c6847ce3ad74e766fb863
drosofff <drosofff@gmail.com>
2020-10-31 17:51:44 +0100

    get pip version in the galaxy .venv environnement with the proper path...

--- travis_scripts/script_ansible.sh
+++ travis_scripts/script_ansible.sh
@@ -13,7 +13,7 @@ export BIOBLEND_TEST_JOB_TIMEOUT=240
 
 ansible-galaxy install -r requirements_roles.yml -p roles
 ansible-playbook -i inventory_files/galaxy-kickstart --skip-tags install_tools galaxy.yml
-sudo -E su $GALAXY_TRAVIS_USER -c "$GALAXY_HOME/galaxy/.venv/pip --version"
+sudo -E su $GALAXY_TRAVIS_USER -c "$GALAXY_HOME/galaxy/.venv/bin/pip --version"
 sleep 60
 ansible-playbook -i inventory_files/galaxy-kickstart --tags install_tools galaxy.yml
 # simple pings to galaxy server
