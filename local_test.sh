e3e85cd7cdc904eb212b5da3ac529659aa19f009
drosofff <drosofff@gmail.com>
2021-04-06 00:16:59 +0200

    real first test of the workflow

--- local_test.sh
+++ local_test.sh
@@ -20,8 +20,8 @@ which pip3
 python3 -m pip install -U pip setuptools 
 python3 -m pip install ansible==2.7.4
 
-# git clone https://github.com/artbio/galaxykickstart -b actions 
-# cd galaxykickstart
+git clone https://github.com/artbio/galaxykickstart -b actions 
+cd galaxykickstart
 
 ansible-galaxy install -r requirements_roles.yml -p roles
 
@@ -39,21 +39,21 @@ curl --fail $BIOBLEND_GALAXY_URL/api/version
 date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://127.0.0.1:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 
 # install bioblend testing, GKS way.
-# pip --version
-# sudo rm -f /etc/boto.cfg
-# pip install --ignore-installed https://github.com/galaxyproject/bioblend/archive/master.zip pytest
-# 
-# chmod a+rx /home/runner/
-# sudo -E su $GALAXY_TRAVIS_USER -c "source /home/travis/virtualenv/python3.7/bin/activate &&
-# cd $GALAXY_HOME &&
-# bioblend-galaxy-tests -v -k 'not download_dataset and \
-#               not download_history and \
-#               not export_and_download and \
-#               not test_show_nonexistent_dataset and \
-#               not test_invocation and \
-#               not test_update_dataset_tags and \
-#               not test_upload_file_contents_with_tags and \
-#               not test_create_local_user and \
-#               not test_show_workflow_versions' \
-#               /home/travis/virtualenv/python3.7/lib/python3.7/site-packages/bioblend/_tests/TestGalaxy*.py"
-# cd $TRAVIS_BUILD_DIR
+pip --version
+sudo rm -f /etc/boto.cfg
+pip install --ignore-installed https://github.com/galaxyproject/bioblend/archive/master.zip pytest
+
+chmod a+rx /home/travis/
+sudo -E su $GALAXY_TRAVIS_USER -c "source /home/travis/virtualenv/python3.7/bin/activate &&
+cd $GALAXY_HOME &&
+bioblend-galaxy-tests -v -k 'not download_dataset and \
+              not download_history and \
+              not export_and_download and \
+              not test_show_nonexistent_dataset and \
+              not test_invocation and \
+              not test_update_dataset_tags and \
+              not test_upload_file_contents_with_tags and \
+              not test_create_local_user and \
+              not test_show_workflow_versions' \
+              /home/travis/virtualenv/python3.7/lib/python3.7/site-packages/bioblend/_tests/TestGalaxy*.py"
+cd $TRAVIS_BUILD_DIR
