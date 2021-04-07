71f049a9ea370934cf0739aa6dec6f16bef482a5
drosofff <drosofff@gmail.com>
2021-04-08 01:43:56 +0200

    pass env var to ansible-playbook

--- test.sh
+++ test.sh
@@ -32,7 +32,7 @@ ansible-galaxy install -r requirements_roles.yml -p roles
 
 # tests
 
-ansible-playbook -i inventory_files/galaxy-kickstart galaxy.yml
+ansible-playbook -i inventory_files/galaxy-kickstart --extra-vars RUNNER_ALLOW_RUNASROOT="1" galaxy.yml
 sleep 15
 # ansible-playbook -i inventory_files/galaxy-kickstart galaxy_tool_install.yml
 
