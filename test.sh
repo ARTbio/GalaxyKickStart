a6a5b4c650bc2a97e88000a873e523f17352d11c
drosofff <drosofff@gmail.com>
2021-04-09 23:41:38 +0200

    lint and restart tests

--- test.sh
+++ test.sh
@@ -16,10 +16,6 @@ export BIOBLEND_GALAXY_API_KEY="artbio2020"
 export BIOBLEND_TEST_JOB_TIMEOUT="240"
 export BIOBLEND_GALAXY_URL="http://127.0.0.1:80"
 
-# /etc/init.d/postgresql stop || true
-# apt-get -y --purge remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common || true
-# rm -rf /var/lib/postgresql || true
-
 
 which pip3
 python3 -m pip install -U pip setuptools 
