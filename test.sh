d98c36c5a79644d42177f9216887d8327fdb6ba3
drosofff <drosofff@gmail.com>
2021-04-11 18:55:23 +0200

    max centralization in PR.yaml

--- test.sh
+++ test.sh
@@ -16,44 +16,8 @@ export BIOBLEND_GALAXY_API_KEY="artbio2020"
 export BIOBLEND_TEST_JOB_TIMEOUT="240"
 export BIOBLEND_GALAXY_URL="http://127.0.0.1:80"
 
-ln -sf /usr/bin/python3.7 python3
-ln -sf /usr/bin/python3.7 python
-
-which pip3
-python3 -m pip install -U pip setuptools 
-python3 -m pip install ansible==2.7.4
-python3 -m pip install --ignore-installed https://github.com/galaxyproject/bioblend/archive/refs/tags/v0.15.0.zip pytest
-
-pip show pytest
-pip show bioblend
-
-
-#debug
-pwd
-
-# run playbooks
-
-ansible-galaxy install -r requirements_roles.yml -p roles
-
-ansible-playbook -i inventory_files/galaxy-kickstart --extra-vars RUNNER_ALLOW_RUNASROOT="1" galaxy.yml
-
-sleep 15
-
-ansible-playbook -i inventory_files/galaxy-kickstart --extra-vars RUNNER_ALLOW_RUNASROOT="1" galaxy_tool_install.yml
-
-# pings galaxy server
-sudo supervisorctl status
-curl http://localhost:80/api/version| grep version_major
-curl --fail $BIOBLEND_GALAXY_URL/api/version
-
-# test proftpd
-echo "\ntest ftp transfer to proftpd server\n"
-date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://127.0.0.1:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
-
-# install bioblend testing environnement
-
 sudo rm -f /etc/boto.cfg # to do: understand the purpose of this step
 
 cd /opt/hostedtoolcache/Python/3.7.10/x64/lib/python3.7/site-packages/bioblend/_tests/
 
-bioblend-galaxy-tests -v --color=yes TestGalaxy*.py || true
+bioblend-galaxy-tests --color=yes TestGalaxy*.py --no-summary || true
