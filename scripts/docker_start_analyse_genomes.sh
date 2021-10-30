66437b93cea8666c47559d427f932984f0d1a2e2
drosofff <drosofff@gmail.com>
2021-10-30 13:34:36 +0200

    Work out docker containerization

---
+++ scripts/docker_start_analyse_genomes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+rm -rf /mnt/galaxy_export /mnt/galaxy_tmp
+mkdir /mnt/galaxy_export /mnt/galaxy_tmp && chown 1450:1450 /mnt/galaxy_export /mnt/galaxy_tmp
+
+export DOCKER_INSTANCE=`docker run -d -p 80:80 -p 21:21 -p 8800:8800 \
+          --privileged=true \
+          -e GALAXY_CONFIG_ALLOW_USER_DATASET_PURGE=True \
+          -e GALAXY_CONFIG_ALLOW_LIBRARY_PATH_PASTE=True \
+          -e GALAXY_CONFIG_ENABLE_USER_DELETION=True \
+          -e GALAXY_CONFIG_ENABLE_BETA_WORKFLOW_MODULES=True \
+          -v /mnt/galaxy_tmp:/tmp \
+          -v /mnt/galaxy_export:/export \
+          artbio/analyse_genomes:2021`
+
+echo "Ctl-C to interrupt logging of the docker instance"
+docker logs -f $DOCKER_INSTANCE
