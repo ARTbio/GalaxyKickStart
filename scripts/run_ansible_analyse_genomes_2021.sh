f3c76885d693f60f77956d8ca1ec6d7f325fd02e
drosofff <drosofff@gmail.com>
2021-11-12 10:33:48 +0100

    Update run_ansible_analyse_genomes_2021.sh

--- scripts/run_ansible_analyse_genomes_2021.sh
+++ scripts/run_ansible_analyse_genomes_2021.sh
@@ -7,16 +7,16 @@ git clone https://github.com/ARTbio/galaxykickstart -b Analyse_genomes
 cd galaxykickstart/
 ansible-galaxy install -r requirements_roles.yml -p roles/ -f
 ansible-playbook -i inventory_files/Analyse_genomes galaxy.yml
-# sleep 15
-# nginx -s reload
-# sleep 15
+sleep 15
+nginx -s reload
+sleep 15
 cd /home/galaxy/tool_dependencies
 rm -rf _conda
-wget https://mydeepseqbucket.s3.amazonaws.com/_conda_tar.gz
-apt install -y pigz
-pigz -dc _conda_tar.gz > _conda
-chown -R galaxy:galaxy _conda
-rm -rf _conda_tar.gz
+#wget https://mydeepseqbucket.s3.amazonaws.com/_conda_tar.gz
+#apt install -y pigz
+#pigz -dc _conda_tar.gz > _conda
+#chown -R galaxy:galaxy _conda
+#rm -rf _conda_tar.gz
 cd ~/galaxykickstart
 ansible-playbook -i inventory_files/Analyse_genomes galaxy_tool_install.yml
 echo "end of deployment\n"
