48aa183b1b75ebecb81d5cf628dc58e2472f49ec
drosofff <drosofff@gmail.com>
2021-11-12 02:57:36 +0100

    Update run_ansible_analyse_genomes_2021.sh

--- scripts/run_ansible_analyse_genomes_2021.sh
+++ scripts/run_ansible_analyse_genomes_2021.sh
@@ -13,7 +13,8 @@ ansible-playbook -i inventory_files/Analyse_genomes galaxy.yml
 cd /home/galaxy/tool_dependencies
 rm -rf _conda
 wget https://mydeepseqbucket.s3.amazonaws.com/_conda_tar.gz
-tar -xf _conda_tar.gz
+apt install -y pigz
+pigz -dc _conda_tar.gz > _conda
 chown -R galaxy:galaxy _conda
 rm -rf _conda_tar.gz
 cd ~/galaxykickstart
