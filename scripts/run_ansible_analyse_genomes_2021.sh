ece63311190db99b842828fc0070981cdc74159d
drosofff <drosofff@gmail.com>
2021-11-16 00:48:23 +0100

    clean scripts

--- scripts/run_ansible_analyse_genomes_2021.sh
+++ scripts/run_ansible_analyse_genomes_2021.sh
@@ -7,15 +7,12 @@ git clone https://github.com/ARTbio/galaxykickstart -b Analyse_genomes
 cd galaxykickstart/
 ansible-galaxy install -r requirements_roles.yml -p roles/ -f
 ansible-playbook -i inventory_files/Analyse_genomes galaxy.yml
-sleep 15
-nginx -s reload
-sleep 15
 cd /home/galaxy/tool_dependencies
 rm -rf _conda
 wget https://mydeepseqbucket.s3.amazonaws.com/_conda.pigz.tar.gz
 apt install -y pigz
 tar -I pigz -xf _conda.pigz.tar.gz
-# rm -rf _conda_tar.gz
+rm -rf _conda_tar.gz
 chown -R galaxy:galaxy _conda
 cd ~/galaxykickstart
 ansible-playbook -i inventory_files/Analyse_genomes galaxy_tool_install.yml
