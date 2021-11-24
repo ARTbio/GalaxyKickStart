d50c9506edfec24fec4925592fee4b051169dfa3
drosofff <drosofff@gmail.com>
2021-11-24 03:33:09 +0100

    patch s3 url in galaxykickstart

--- scripts/run_ansible_analyse_genomes_2021.sh
+++ scripts/run_ansible_analyse_genomes_2021.sh
@@ -9,7 +9,7 @@ ansible-galaxy install -r requirements_roles.yml -p roles/ -f
 ansible-playbook -i inventory_files/Analyse_genomes galaxy.yml
 cd /home/galaxy/tool_dependencies
 rm -rf _conda
-wget https://mydeepseqbucket.s3.amazonaws.com/_conda.pigz.tar.gz
+wget https://analyse-genomes.s3.eu-west-3.amazonaws.com/_conda.pigz.tar.gz
 apt install -y pigz
 tar -I pigz -xf _conda.pigz.tar.gz
 rm -rf _conda_tar.gz
